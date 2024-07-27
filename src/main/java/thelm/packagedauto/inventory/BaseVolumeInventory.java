2be267165ced15cfb431e14448eef6d44f2b9be7
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-07-27 20:43:27 +0800

    Update BaseVolumeInventory.java

--- src/main/java/thelm/packagedauto/inventory/BaseVolumeInventory.java
+++ src/main/java/thelm/packagedauto/inventory/BaseVolumeInventory.java
@@ -46,7 +46,7 @@ public class BaseVolumeInventory {
 	}
 
 	public void load(CompoundTag nbt) {
-		Arrays.fill(stacks, FluidStack.EMPTY);
+		Arrays.fill(stacks, type.getEmptyStackInstance());
 		ListTag tagList = nbt.getList("Volumes", 10);
 		for(int i = 0; i < tagList.size(); ++i) {
 			CompoundTag tag = tagList.getCompound(i);
