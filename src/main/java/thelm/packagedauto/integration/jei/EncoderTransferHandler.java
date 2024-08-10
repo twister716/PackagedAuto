abbb5e4396ef935827bb95a35521825814f000b3
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-08-11 01:26:56 +0800

    Update JEI

--- src/main/java/thelm/packagedauto/integration/jei/EncoderTransferHandler.java
+++ src/main/java/thelm/packagedauto/integration/jei/EncoderTransferHandler.java
@@ -8,8 +8,8 @@ import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.transfer.IRecipeTransferError;
-import mezz.jei.api.recipe.transfer.IRecipeTransferHandler;
 import mezz.jei.api.recipe.transfer.IRecipeTransferHandlerHelper;
+import mezz.jei.api.recipe.transfer.IUniversalRecipeTransferHandler;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.MenuType;
@@ -19,7 +19,7 @@ import thelm.packagedauto.api.IPackageRecipeType;
 import thelm.packagedauto.menu.EncoderMenu;
 import thelm.packagedauto.packet.SetRecipePacket;
 
-public class EncoderTransferHandler implements IRecipeTransferHandler<EncoderMenu, Object> {
+public class EncoderTransferHandler implements IUniversalRecipeTransferHandler<EncoderMenu> {
 
 	private final IRecipeTransferHandlerHelper transferHelper;
 
@@ -35,11 +35,6 @@ public class EncoderTransferHandler implements IRecipeTransferHandler<EncoderMen
 	@Override
 	public Optional<MenuType<EncoderMenu>> getMenuType() {
 		return Optional.empty();
-	}
-
-	@Override
-	public RecipeType<Object> getRecipeType() {
-		return null;
 	}
 
 	@Override
